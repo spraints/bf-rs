3b60428c7f71e63106b078326213f684d83af235
Matt Burke <spraints@gmail.com>
2024-07-25 12:09:32 -0400

    hello world works!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,82 @@
 use std::{
+    env::{args, Args},
     error::Error,
+    fs::File,
     io::{stdin, BufReader, Read},
 };
 
 fn main() {
-    if let Err(e) = run(Default::default(), stdin().lock()) {
+    if let Err(e) = main_impl(args()) {
         eprintln!("error: {e}");
     }
 }
 
-fn run(mut m: Machine, r: impl Read) -> Result<(), Box<dyn Error>> {
-    let r = BufReader::new(r);
-    for b in r.bytes() {
-        match parse(b?) {
-            None => (),
-            Some(x) => m.apply(x),
-        };
+fn main_impl(args: Args) -> Result<(), Box<dyn Error>> {
+    let (src, input) = parse_args(args)?;
+    let mut program = Vec::new();
+    let src = BufReader::new(src);
+    for c in src.bytes() {
+        if let Some(cmd) = parse(c?) {
+            program.push(cmd);
+        }
+    }
+    run(Default::default(), &program, input)
+}
+
+fn parse_args(mut args: Args) -> Result<(Box<dyn Read>, Box<dyn Read>), Box<dyn Error>> {
+    let exe = args.next().ok_or("expected program name at args[0]")?;
+    let program_filename = args
+        .next()
+        .ok_or_else(|| format!("Usage: {exe} PROGRAM <INPUT"))?;
+    Ok((
+        Box::new(File::open(&program_filename)?),
+        Box::new(stdin().lock()),
+    ))
+}
+
+fn run(mut m: Machine, program: &[Command], r: impl Read) -> Result<(), Box<dyn Error>> {
+    let mut pc = 0;
+    let mut input = r.bytes();
+    while pc < program.len() {
+        let cmd = program[pc];
+        pc += 1;
+        match cmd {
+            Command::IncrementDataPointer => m.p += 1,
+            Command::DecrementDataPointer => m.p -= 1,
+            Command::IncrementByte => m.cells[m.p] += 1,
+            Command::DecrementByte => m.cells[m.p] -= 1,
+            Command::OutputByte => print!("{}", m.char_at_p()),
+            Command::AcceptOneByte => {
+                m.cells[m.p] = input.next().ok_or("[{pc}] unexpected end of input")??
+            }
+            Command::JumpForward => {
+                if m.cells[m.p] == 0 {
+                    let mut nesting = 1;
+                    while nesting > 0 {
+                        let cmd = program[pc];
+                        pc += 1;
+                        match cmd {
+                            Command::JumpForward => nesting += 1,
+                            Command::JumpBackward => nesting -= 1,
+                            _ => (),
+                        };
+                    }
+                }
+            }
+            Command::JumpBackward => {
+                if m.cells[m.p] != 0 {
+                    let mut nesting = 1;
+                    while nesting > 0 {
+                        pc -= 1;
+                        match program[pc - 1] {
+                            Command::JumpForward => nesting -= 1,
+                            Command::JumpBackward => nesting += 1,
+                            _ => (),
+                        };
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
@@ -34,6 +95,7 @@ fn parse(b: u8) -> Option<Command> {
     }
 }
 
+#[derive(Clone, Copy)]
 enum Command {
     IncrementDataPointer,
     DecrementDataPointer,
@@ -60,27 +122,6 @@ impl Default for Machine {
 }
 
 impl Machine {
-    fn apply(&mut self, c: Command) {
-        match c {
-            Command::IncrementDataPointer => self.p += 1,
-            Command::DecrementDataPointer => self.p -= 1,
-            Command::IncrementByte => self.cells[self.p] += 1,
-            Command::DecrementByte => self.cells[self.p] -= 1,
-            Command::OutputByte => print!("{}", self.char_at_p()),
-            Command::AcceptOneByte => todo!("accept one byte of input (',')"),
-            Command::JumpForward => {
-                if self.cells[self.p] == 0 {
-                    todo!("jump forward ('[') to ']'");
-                }
-            }
-            Command::JumpBackward => {
-                if self.cells[self.p] != 0 {
-                    todo!("jump backward (']') to '['");
-                }
-            }
-        };
-    }
-
     fn char_at_p(&self) -> char {
         self.cells[self.p] as char
     }
